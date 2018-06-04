d3a89db7ad809d12ce9ccb5f2cfe60542a4ecf89
Алексей <rylovalex2108l@gmail.com>
2018-06-04 17:59:15 +0700

    Внес исправления по 10 заданию

--- js/stat.js
+++ js/stat.js
@@ -19,46 +19,29 @@ var getColors = function () {
   return 'rgba(0, 0, 255, ' + val + ')';
 };
 
+
+// функция для получения максимального времени
 var getMaxTime = function (arr) {
-  var max = arr[0];
-  for (var i = 1; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  return max;
+  return Math.max.apply(null, arr);
 };
 
 var getCloud = function (ctx, x, y, color) {
+  // массив с координатами по оси X
+  var arrX = [x, 400, 520, 500, 520, 220, 100, 120, x];
+  // массив с координатами по оси Y
+  var arrY = [y, 30, 10, 200, 280, 260, 280, 90, y];
   ctx.fillStyle = color;
   ctx.beginPath();
   ctx.moveTo(x, y);
-  ctx.lineTo(x + 300, y + 20);
-  ctx.lineTo(x + 420, y);
-  ctx.lineTo(x + 400, y + 190);
-  ctx.lineTo(x + 420, y + 270);
-  ctx.lineTo(x + 120, y + 250);
-  ctx.lineTo(x, y + 270);
-  ctx.lineTo(x + 20, y + 80);
-  ctx.lineTo(x, y);
+  for (var i = 0; i < arrX.length; i++) {
+    ctx.lineTo(arrX[i], arrY[i]);
+  }
   ctx.closePath();
-  ctx.stroke();
   ctx.fill();
 };
 
-window.renderStatistics = function (ctx, names, times) {
-
-  getCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  getCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
-
+var renderStatsBar = function (ctx, names, times) {
   var maxTime = getMaxTime(times);
-
-  ctx.fillStyle = COLOR_BLACK;
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 4);
 
   for (var i = 0; i < names.length; i++) {
     // высота каждого прямоугольника
@@ -67,7 +50,7 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillStyle = COLOR_BLACK;
     ctx.fillText(names[i], barWidth + (TEXT_WIDTH + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP * 3);
 
-    // если есть я - то рисуем и пропускаем цикл, чтобы еще раз не прорисовывать под этим рисунком копию
+    // если есть 'Вы' - то рисуем и пропускаем цикл, чтобы еще раз не прорисовывать под этим рисунком одну копию себя
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
       ctx.fillRect(barWidth + (TEXT_WIDTH + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP * 4, BAR_WIDTH, -barHeight);
@@ -82,5 +65,18 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillStyle = COLOR_BLACK;
     ctx.fillText(Math.round(times[i]), barWidth + (TEXT_WIDTH + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP * 6 - barHeight);
   }
+};
 
+window.renderStatistics = function (ctx, names, times) {
+
+  getCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  getCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = COLOR_BLACK;
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 4);
+
+  renderStatsBar(ctx, names, times);
 };
