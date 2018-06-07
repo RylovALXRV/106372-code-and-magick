715a79bbe714308108d54edecc1805a0c6c23264
Алексей <rylovalex2108l@gmail.com>
2018-06-07 23:03:42 +0700

    создал функции, убрал лишние вызовы переменных, доработал код

--- js/setup.js
+++ js/setup.js
@@ -1,19 +1,14 @@
-/* eslint-disable guard-for-in,no-shadow */
 'use strict';
 
+// без .content. не работает - я в демке взял, да и на лекции показывали, по-другому не знаю как
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').
-content.querySelector('.setup-similar-item');
-
-var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
-
-var setupSimilar = document.querySelector('.setup-similar');
-setupSimilar.classList.remove('hidden');
-
-var setupSimilarList = setupSimilar.querySelector('.setup-similar-list');
+document.querySelector('.setup').classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
 
 var fragment = document.createDocumentFragment();
+
+var wizards = [];
 
 var wizardNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия',
   'Люпита', 'Вашингтон'];
@@ -26,85 +21,41 @@ var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)
 
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var getRandomElem = function (arr) {
+var getRandomFeature = function (arr) {
   var randValue = Math.floor(Math.random() * arr.length);
   return arr[randValue];
 };
 
-// var fullNameElem = getRandomElem(wizardNames) + ' ' + getRandomElem(wizardSurnames);
-
-
-// МОЖНО ЛИ ТАК ВООБЩЕ ДЕЛАТЬ??
-// создал один пустой объект в массиве для копирования его свойств
-var wizards = [
-  {
-    name: '',
-    coatColor: '',
-    eyesColor: ''
-  }
-];
-
-// создаю копию объекта и возвращаю его
-var copyWizards = function (obj) {
-  var clone = {};
-  for (var key in obj) { // а здесь вообще ругается на перебор объекта через for-in
-    clone[key] = obj[key];
-  }
-  return clone;
+// создал одного волшебника
+var createWizard = function () {
+  var obj = {
+    name: getRandomFeature(wizardNames) + ' ' + getRandomFeature(wizardSurnames),
+    coatColor: getRandomFeature(coatColors),
+    eyesColor: getRandomFeature(eyesColors)
+  };
+  return obj;
 };
 
-// делаю количество копий и добавляю в массив объекты
-var makeElems = function (arr, num) {
-  for (var i = 0; i < num - 1; i++) {
-    var elem = copyWizards(arr[i]);
-    arr.push(elem);
-  }
-  return arr;
-};
-
-makeElems(wizards, 4);
-
-// задаю для каждого из объекта свои свойства
-for (var i = 0; i < wizards.length; i++) {
-  wizards[i].name = getRandomElem(wizardNames) + ' ' + getRandomElem(wizardSurnames);
-  wizards[i].coatColor = getRandomElem(coatColors);
-  wizards[i].eyesColor = getRandomElem(eyesColors);
+// сгенерировал массив из 4-х объектов(волшебников)
+for (var i = 0; i < 4; i++) {
+  wizards.push(createWizard());
 }
-// var wizards = [
-//   {
-//     name: fullNameElem,
-//     coatColor: getRandomElem(coatColors),
-//     eyesColor: getRandomElem(eyesColors)
-//   },
-//   {
-//     name: fullNameElem,
-//     coatColor: getRandomElem(coatColors),
-//     eyesColor: getRandomElem(eyesColors)
-//   },
-//   {
-//     name: fullNameElem,
-//     coatColor: getRandomElem(coatColors),
-//     eyesColor: getRandomElem(eyesColors)
-//   },
-//   {
-//     name: fullNameElem,
-//     coatColor: getRandomElem(coatColors),
-//     eyesColor: getRandomElem(eyesColors)
-//   }
-// ];
-//
 
-// задаю для каждого из волшебника свойства и вставляю фрагмент на страницу
-var getSimilarElement = function (arr) {
-  for (var i = 0; i < arr.length; i++) {
-    var wizardElement = similarWizardTemplate.cloneNode(true);
-    wizardElement.querySelector('.setup-similar-label').textContent = arr[i].name;
-    wizardElement.querySelector('.wizard-coat').style.fill = arr[i].coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = arr[i].eyesColor;
-    fragment.appendChild(wizardElement);
-  }
-
-  return setupSimilarList.appendChild(fragment);
+// создал волшебника со всеми его свойствами на странице
+var renderWizard = function (arr) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = arr.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = arr.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = arr.eyesColor;
+  return wizardElement;
 };
 
-getSimilarElement(wizards);
+// создал для всех волшебников свойства
+var renderSimilarWizards = function (arr) {
+  for (var j = 0; j < arr.length; j++) {
+    fragment.appendChild(renderWizard(arr[j]));
+  }
+  return document.querySelector('.setup-similar-list').appendChild(fragment);
+};
+
+renderSimilarWizards(wizards);
