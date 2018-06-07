700391d0f3fc15c50fc65ff6af8f4af7401bae1d
Алексей <rylovalex2108l@gmail.com>
2018-06-07 18:08:04 +0700

    поправил функцию renderStatsBar с учетом новых передаваемых параметров, поправил статические переменные.

--- js/stat.js
+++ js/stat.js
@@ -7,9 +7,9 @@ var COLOR_BLACK = '#000000';
 var GAP = 10;
 var TEXT_WIDTH = 50;
 var BAR_WIDTH = 40;
-var barHeight = 0;
-var barWidth = CLOUD_X + TEXT_WIDTH;
-var barHeightMax = 150;
+var BAR_LEFT_BEGIN = CLOUD_X + TEXT_WIDTH;
+var BAR_AND_TEXT_WIDTH = TEXT_WIDTH + BAR_WIDTH;
+var BAR_HEIGHT_MAX = 150;
 
 var getColors = function () {
   var val = (Math.round(Math.random() * 10)) / 10;
@@ -58,33 +58,31 @@ var getCloud = function (ctx, x, y, color) {
   ctx.fill();
 };
 
-var renderStatsBar = function (ctx, i, names, times) {
-  var maxTime = getMaxTime(times);
-  barHeight = Math.round(times[i] * barHeightMax / maxTime);
-  // имена всех участников
+var renderStatsBar = function (ctx, data, cords) {
   ctx.fillStyle = COLOR_BLACK;
-  ctx.fillText(names[i], barWidth + (TEXT_WIDTH + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP * 3);
-  // высота столбцов
-  if (names[i] === 'Вы') {
+  ctx.fillText(data.name, cords.x, cords.y - GAP * 3);
+  if (data.name === 'Вы') {
     ctx.fillStyle = 'rgba(255, 0, 0, 1)';
   } else {
     ctx.fillStyle = getColors();
   }
-  ctx.fillRect(barWidth + (TEXT_WIDTH + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP * 4, BAR_WIDTH, -barHeight);
-  // вывод времени
+  ctx.fillRect(cords.x, cords.y - GAP * 4, BAR_WIDTH, -data.timePercentage);
   ctx.fillStyle = COLOR_BLACK;
-  ctx.fillText(Math.round(times[i]), barWidth + (TEXT_WIDTH + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP * 6 - barHeight);
-
+  ctx.fillText(Math.round(data.time), cords.x, cords.y - GAP * 6 - data.timePercentage);
 };
 
 window.renderStatistics = function (ctx, names, times) {
+  // вызываю функцию с максимальным временем и записываю в переменную
+  var maxTime = getMaxTime(times);
   getCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   getCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   displayTextCloud(ctx, 'Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP + GAP / 2);
   displayTextCloud(ctx, 'Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 3 + GAP / 2);
+
   // вывод на экран статистики всех игроков
   for (var i = 0; i < names.length; i++) {
-    renderStatsBar(ctx, i, names, times);
+    // в функцию передаю объекты с данными и координатами
+    renderStatsBar(ctx, {name: names[i], time: times[i], timePercentage: Math.round(times[i] * BAR_HEIGHT_MAX / maxTime)}, {x: BAR_LEFT_BEGIN + BAR_AND_TEXT_WIDTH * i, y: CLOUD_HEIGHT});
   }
 };
